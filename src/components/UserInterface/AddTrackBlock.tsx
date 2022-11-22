e53c124452c11c7d5200f10b0102806c11991699
GRND CTRL <mjrtom@grndctrl.io>
2022-11-22 16:39:51 +0000

    add track using block positions

--- src/components/UserInterface/AddTrackBlock.tsx
+++ src/components/UserInterface/AddTrackBlock.tsx
@@ -2,52 +2,103 @@ import { TrackModel } from 'models/TrackModel';
 import { TrackAngle, TrackBlockType, TrackDirection, TrackVariation } from 'core/Track';
 import { useTrack } from 'core/Track/hooks';
 import { useMouseControls } from 'hooks/mouseControls';
-import { useEffect, useMemo } from 'react';
-import { Vector3 } from 'three';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
+import { BufferGeometry, Vector3 } from 'three';
 import { BlockType } from 'utils/blockUtils';
 import { blockSize } from 'utils/constants';
 import { useInterfaceStore } from 'utils/interfaceStore';
 import { UISelection } from 'utils/interfaceUtils';
+import { Arrow } from 'models/Arrow';
 
 type AddTrackBlockProps = {
-  block: BlockType;
+  blockHovered: {
+    block: BlockType;
+    vertex: { index: number; position: Vector3 };
+    geometry: BufferGeometry;
+  };
+  neighbours: {
+    block: BlockType;
+    position: Vector3;
+    geometry: BufferGeometry;
+  }[];
 };
 
-const AddTrackBlock = ({ block }: AddTrackBlockProps) => {
+const AddTrackBlock = ({ blockHovered, neighbours }: AddTrackBlockProps) => {
   const position = useMemo(
-    () => block.worldPosition.clone().add(new Vector3(0, blockSize.y, 0)),
-    [block.worldPosition]
+    () => blockHovered.block.worldPosition.clone().add(new Vector3(0, blockSize.y, 0)),
+    [blockHovered.block.worldPosition],
   );
 
-  const { setCurrUISelection } = useInterfaceStore();
+  const blocks = useRef<BlockType[]>([]);
+  const blocksFrom = useRef<BlockType[]>([]);
+  const blocksTo = useRef<BlockType[]>([]);
+
+  const { setCurrUISelection, trackSettings } = useInterfaceStore();
 
   const { length, setBlock } = useTrack();
 
   const { leftButton } = useMouseControls();
 
-  useEffect(() => {
-    if (leftButton && length === 0) {
-      const trackBlock: TrackBlockType = {
-        id: 0,
-        worldPosition: position,
-        direction: {
-          from: TrackDirection.SOUTH,
-          to: TrackDirection.NORTH,
-          angle: TrackAngle.STRAIGHT,
-        },
-        track: {
-          variation: TrackVariation.FORWARD,
-          isPartial: false,
-        },
-      };
-      setBlock(trackBlock);
-      setCurrUISelection(UISelection.EDITROAD);
-
-      console.log('tick');
+  const sortBlocks = useCallback(() => {
+    if (blockHovered.vertex.index === 0 || blockHovered.vertex.index === 4) {
+      return [neighbours[0].block, neighbours[1].block, neighbours[2].block, blockHovered.block];
+    } else if (blockHovered.vertex.index === 1 || blockHovered.vertex.index === 5) {
+      return [neighbours[0].block, neighbours[1].block, blockHovered.block, neighbours[2].block];
+    } else if (blockHovered.vertex.index === 2 || blockHovered.vertex.index === 6) {
+      return [neighbours[0].block, blockHovered.block, neighbours[1].block, neighbours[2].block];
     }
-  }, [leftButton, position]);
 
-  return <TrackModel variation={TrackVariation.FORWARD} angle={TrackAngle.STRAIGHT} position={position} scale={10} />;
+    return [blockHovered.block, neighbours[0].block, neighbours[1].block, neighbours[2].block];
+  }, [blockHovered.block, blockHovered.vertex.index, neighbours]);
+
+  useEffect(() => {
+    if (neighbours.length < 3) {
+      blocks.current = [];
+      blocksFrom.current = [];
+      blocksTo.current = [];
+      return;
+    }
+
+    blocks.current = sortBlocks();
+
+    if (trackSettings.direction.from === TrackDirection.NORTH) {
+      blocksFrom.current = [blocks.current[1], blocks.current[0]];
+      blocksTo.current = [blocks.current[3], blocks.current[2]];
+    } else if (trackSettings.direction.from === TrackDirection.WEST) {
+      blocksFrom.current = [blocks.current[0], blocks.current[2]];
+      blocksTo.current = [blocks.current[1], blocks.current[3]];
+    } else if (trackSettings.direction.from === TrackDirection.SOUTH) {
+      blocksFrom.current = [blocks.current[2], blocks.current[3]];
+      blocksTo.current = [blocks.current[0], blocks.current[1]];
+    } else if (trackSettings.direction.from === TrackDirection.EAST) {
+      blocksFrom.current = [blocks.current[3], blocks.current[1]];
+      blocksTo.current = [blocks.current[2], blocks.current[0]];
+    }
+  }, [neighbours.length, sortBlocks, trackSettings.direction.from]);
+
+  //   useEffect(() => {
+  //     if (leftButton && length === 0) {
+  //       const trackBlock: TrackBlockType = {
+  //         id: 0,
+  //         worldPosition: position,
+  //         direction: {
+  //           from: TrackDirection.SOUTH,
+  //           to: TrackDirection.NORTH,
+  //           angle: TrackAngle.STRAIGHT,
+  //         },
+  //         track: {
+  //           variation: TrackVariation.FORWARD,
+  //           isPartial: false,
+  //         },
+  //       };
+  //       setBlock(trackBlock);
+  //       setCurrUISelection(UISelection.EDITROAD);
+  //
+  //       console.log('tick');
+  //     }
+  //   }, [leftButton, position]);
+
+  return <Arrow position={position} />;
 };
 
 export default AddTrackBlock;
